b4d0d28a11ec9b66af157082c0be859b2c86adb0
Yohei Shimmyo <qwpmb554@gmail.com>
2024-03-03 16:40:18 +0900

    add debug macros

---
+++ firmware/src/debug/fmt.h
@@ -0,0 +1,15 @@
+#ifndef __FMT_H__
+#define __FMT_H__
+#include <avr/io.h>
+
+#define DEBUG_SEND_STR(msg)                                                    \
+  {                                                                            \
+    const uint8_t m[] = (msg);                                                 \
+    debug_send_word(m, sizeof(m) / sizeof(m[0]));                              \
+  }
+
+void debug_send_byte(const uint8_t);
+void debug_send_word(const uint8_t *, const uint8_t);
+void debug_send_hex(const uint8_t);
+
+#endif /* __FMT_H__ */
